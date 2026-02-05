1b2f97391b76a319b08ebaa2b446f418e02f03ba
jazzrail <jazzrail@hotmail.com>
2026-02-05 21:04:57 +0000

    Started JavaScript to fetch quotes

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+  // Pick a random qoute from api quotes array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+}
+
 // Get Quotes From API
 async function getQuotes() {
-  ...
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch error here
+  }
+}
+
+// On Load
+getQuotes();
